ca42ba858f14c0e95d3e5a2b4ad615c1c02f579a
Aucke Bos <aucke.bos97@gmail.com>
2023-08-24 21:15:16 +0200

    Added abstaract extractor

---
+++ bunq_ynab_connect/data/data_extractors/abstract_data_extractor.py
@@ -0,0 +1,35 @@
+from abc import ABC
+from dataclasses import dataclass
+from logging import LoggerAdapter
+from typing import Iterable
+
+from kink import inject
+
+from bunq_ynab_connect.data.storage.abstract_storage import AbstractStorage
+
+
+class AbstractDataExtractors(ABC):
+    destination: str
+    storage: AbstractStorage
+    logger: LoggerAdapter
+
+    @inject
+    def __init__(
+        self, destination: str, storage: AbstractStorage, logger: LoggerAdapter
+    ) -> None:
+        self.destination = destination
+        self.storage = storage
+        self.logger = logger
+
+    def load(self) -> Iterable:
+        """
+        Load the data from the source.
+        """
+        raise NotImplementedError
+
+    def extract(self) -> None:
+        """
+        Extract the data from the bunq API, and save it in self.data.
+        """
+        data = self.load()
+        self.storage.upsert(data, self.destination)
