28bdf67b8377d913d57c38e8a4100eff39226e3b
Aucke Bos <aucke.bos97@gmail.com>
2023-08-28 21:39:59 +0200

    Moved metadata dir

--- bunq_ynab_connect/helpers/config.py
+++ bunq_ynab_connect/helpers/config.py
@@ -6,8 +6,8 @@ from pathlib import Path
 
 PROJECT_DIR = Path(os.path.dirname(os.path.abspath(__file__))) / ".."
 CONFIG_DIR = PROJECT_DIR / ".." / "config"
-BUNQ_CONFIG_FILE = f"{CONFIG_DIR}/bunq.cfg"
-METADATA_DIR = CONFIG_DIR / "metadata"
+BUNQ_CONFIG_FILE = CONFIG_DIR / "bunq.cfg"
+METADATA_DIR = PROJECT_DIR / "metadata"
 LOGS_DIR = PROJECT_DIR / ".." / "logs"
 CACHE_DIR = PROJECT_DIR / ".." / "cache"
 LOGS_FILE = LOGS_DIR / "logs.log"
