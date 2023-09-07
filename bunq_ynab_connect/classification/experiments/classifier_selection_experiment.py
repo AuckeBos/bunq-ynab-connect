968ba9d8edf01f807588c1e1d1f303281824b407
Aucke Bos <aucke.bos97@gmail.com>
2023-09-07 20:19:54 +0200

    Fix multiprocessing issues

--- bunq_ynab_connect/classification/experiments/classifier_selection_experiment.py
+++ bunq_ynab_connect/classification/experiments/classifier_selection_experiment.py
@@ -1,7 +1,9 @@
+import multiprocessing
 from multiprocessing.pool import ThreadPool
 
 import numpy as np
 from interpret.glassbox import ExplainableBoostingClassifier
+from mlflow.client import MlflowClient
 from prefect import task
 from sklearn.base import ClassifierMixin
 from sklearn.calibration import LabelEncoder
@@ -44,22 +46,32 @@ class ClassifierSelectionExperiment(BasePaymentClassificationExperiment):
 
     def _run(self, X: np.ndarray, y: np.ndarray):
         """
-        Run the experiment.
+        Run the experiment. Use a Thread Pool to run the classifiers in parallel.
+        The amount of threads is the amount of CPU cores minus 1.
         """
 
         def run_for_one_classifier(
             self, classifier: ClassifierMixin, X: np.ndarray, y: np.ndarray
         ):
-            mlflow.set_experiment(self.get_experiment_name())
-            # Todo: This nests inside other runs
-            with mlflow.start_run(
-                run_name=classifier.__class__.__name__,
-                nested=True,
+            """
+            Run the experiment for a single classifier.
+            Use the client instead of mlflow, to prevent threading issues and non-closed runs.
+            """
+            client = MlflowClient()
+            experiment_id = client.get_experiment_by_name(
+                self.get_experiment_name()
+            ).experiment_id
+            run = client.create_run(
+                experiment_id=experiment_id,
                 tags={"mlflow.parentRunId": self.run_id},
-            ) as run:
+                run_name=classifier.__class__.__name__,
+            )
+            try:
                 self.run_classifier(classifier, X, y)
+            finally:
+                client.set_terminated(run.info.run_id)
 
-        pool = ThreadPool(processes=4)
+        pool = ThreadPool(processes=multiprocessing.cpu_count() - 1)
         pool.map(
             lambda classifier: run_for_one_classifier(self, classifier, X, y),
             self.CLASSIFIERS,
@@ -68,6 +80,9 @@ class ClassifierSelectionExperiment(BasePaymentClassificationExperiment):
     def run_classifier(self, model: ClassifierMixin, X: np.ndarray, y: np.ndarray):
         """
         Run the experiment for a single classifier.
+        - Create the pipeline
+        - Use Kfold
+        - Score and log the mean score
         """
         classifier = self.create_pipeline(model)
         k_fold = StratifiedKFold(
